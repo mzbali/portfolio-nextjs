0f1983c5a58fc19df449112c964e2e2844c03d91
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-11-17 03:44:04 +0600

    initial project setup and configuring nextjs, prettier and eslint config files and first hello

---
+++ README.md
@@ -0,0 +1,3 @@
+# portfolio-nextjs
+
+My portfolio Project
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const option = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: true,
+    tabWidth: 4,
+    trailingComma: 'none'
+};
+
+module.exports = option;
