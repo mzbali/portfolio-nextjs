f9cd3a522064b940c476e7080e373c755ba219e2
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-11-18 22:23:02 +0600

    changed font of logo and header to M PLUS Rounded 1c

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../component/layouts/main';
 import theme from '../lib/theme';
+import Fonts from '../component/Fonts';
 
 const Website: React.FC = ({ Component, pageProps, router }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
