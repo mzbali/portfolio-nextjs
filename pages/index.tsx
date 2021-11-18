bbfb9e454affd5a02cc2a3cd876176b92228b08a
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-11-18 23:54:47 +0600

    Added animated profile section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import {
   Image,
   useColorModeValue
 } from '@chakra-ui/react';
-
+import Section from '../component/Section';
 const Page: React.FC = () => {
   return (
     <Container>
@@ -44,6 +44,12 @@ const Page: React.FC = () => {
           />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   );
 };
