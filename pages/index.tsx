f9815c59c093a365166433d4f1ff02ab2994c172
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-11-19 22:08:32 +0600

    implemented page transition in and out using framer motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,96 +9,99 @@ import {
   Button
 } from '@chakra-ui/react';
 import Section from '../components/Section';
+import Layout from '../components/layouts/Article';
 import Paragraph from '../components/Paragraph';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { BioSection, BioYear } from '../components/Bio';
 
 const Page: React.FC = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        textAlign="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Bangladesh!
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Mahmud Zaman Bali
-          </Heading>
-          <p>Arsenal Fan ( Bookphile / Developer / Designer )</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          align="center"
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          textAlign="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/mahmud.jpg"
-            alt="Profile Image"
-          />
+          Hello, I&apos;m a full-stack developer based in Bangladesh!
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          Mahmud is a freelance and a full-stack developer based in Dhaka with a
-          passion for building digital services/stuff he wants, He has a knack
-          for all things launching products, from planning and designing all way
-          to solving real-life problems with hanging out with his camera.
-          Currently, he is living off of his own prooduct called {''}
-          <NextLink href="/works/inkdrop">
-            <Link>Inkdrop</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My Portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Mahmud Zaman Bali
+            </Heading>
+            <p>Arsenal Fan ( Bookphile / Developer / Designer )</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/mahmud.jpg"
+              alt="Profile Image"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1998</BioYear>
-          Born in Madaripur, Bangladesh.
-        </BioSection>
-        <BioSection>
-          <BioYear>2021</BioYear>
-          Completed Undergraduate degree&apos;s in Computer Science and
-          Engineering at American International University-Bangladesh
-        </BioSection>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I 🤍
-        </Heading>
-        <Paragraph>
-          History, Music,{' '}
-          <Link href="https://illust.oduruinu.net/">Drawing</Link>, Playing
-          Drums, <Link href="https://500px.com/p/craftzdog">Photography</Link>,
-          Linux, Machine Learning and All things technology
-        </Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Mahmud is a freelance and a full-stack developer based in Dhaka with
+            a passion for building digital services/stuff he wants, He has a
+            knack for all things launching products, from planning and designing
+            all way to solving real-life problems with hanging out with his
+            camera. Currently, he is living off of his own prooduct called {''}
+            <NextLink href="/works/inkdrop">
+              <Link>Inkdrop</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My Portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1998</BioYear>
+            Born in Madaripur, Bangladesh.
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Completed Undergraduate degree&apos;s in Computer Science and
+            Engineering at American International University-Bangladesh
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I 🤍
+          </Heading>
+          <Paragraph>
+            History, Music,{' '}
+            <Link href="https://illust.oduruinu.net/">Drawing</Link>, Playing
+            Drums, <Link href="https://500px.com/p/craftzdog">Photography</Link>
+            , Linux, Machine Learning and All things technology
+          </Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   );
 };
 
--- pages/works.tsx
+++ pages/works.tsx
@@ -1,6 +1,8 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react';
 import Section from '../components/Section';
 import { WorkGridItem } from '../components/GridItem';
+import Layout from '../components/layouts/Article';
+
 import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png';
 import thumbWalknote from '../public/images/works/walknote_eyecatch.png';
 import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg';
@@ -13,97 +15,103 @@ import thumbAmembo from '../public/images/works/amembo_eyecatch.png';
 
 const Works: React.FC = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.1}>
-          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
-            A markdown note-taking app
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="walknote"
-            title="Walknote"
-            thumbnail={thumbWalknote}
-          >
-            Music reccomendation app for ios
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem id="menkiki" thumbnail={thumbMenkiki} title="Menkiki">
-            An app that suggests ramen(noodle) shops based on a given photo of
-            the ramen you want to eat
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="fourpainters"
-            title="The four painters"
-            thumbnail={thumbFourPainters}
-          >
-            A video work generated with deep learning, imitating famous four
-            painters like Van Gogh
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-      <Section delay={0.2}>
-        <Divider my={6} />
-
+    <Layout>
+      <Container>
         <Heading as="h3" fontSize={20} mb={4}>
-          Collaborations
+          Works
         </Heading>
-      </Section>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem
-            id="modetokyo"
-            thumbnail={thumbModeTokyo}
-            title="mode.tokyo"
-          >
-            The mode magazine for understanding to personally enjoy Japan
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.3}>
-          <WorkGridItem id="styly" thumbnail={thumbStyly} title="Styly">
-            A VR Creative tools for fashion brands
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.1}>
+            <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
+              A markdown note-taking app
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="walknote"
+              title="Walknote"
+              thumbnail={thumbWalknote}
+            >
+              Music reccomendation app for ios
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.1}>
+            <WorkGridItem id="menkiki" thumbnail={thumbMenkiki} title="Menkiki">
+              An app that suggests ramen(noodle) shops based on a given photo of
+              the ramen you want to eat
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="fourpainters"
+              title="The four painters"
+              thumbnail={thumbFourPainters}
+            >
+              A video work generated with deep learning, imitating famous four
+              painters like Van Gogh
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+        <Section delay={0.2}>
+          <Divider my={6} />
 
-      <Section delay={0.4}>
-        <Divider my={6} />
+          <Heading as="h3" fontSize={20} mb={4}>
+            Collaborations
+          </Heading>
+        </Section>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.3}>
+            <WorkGridItem
+              id="modetokyo"
+              thumbnail={thumbModeTokyo}
+              title="mode.tokyo"
+            >
+              The mode magazine for understanding to personally enjoy Japan
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.3}>
+            <WorkGridItem id="styly" thumbnail={thumbStyly} title="Styly">
+              A VR Creative tools for fashion brands
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
 
-        <Heading as="h3" fontSize={20} mb={4}>
-          Old works
-        </Heading>
-      </Section>
+        <Section delay={0.4}>
+          <Divider my={6} />
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.5}>
-          <WorkGridItem id="pichu2" thumbnail={thumbPichu2} title="Pichu*Pichu">
-            Twitter client app for iPhone Safari
-          </WorkGridItem>
+          <Heading as="h3" fontSize={20} mb={4}>
+            Old works
+          </Heading>
         </Section>
-        <Section delay={0.5}>
-          <WorkGridItem
-            id="freedbtagger"
-            thumbnail={thumbFreeDBTagger}
-            title="freeDBTagger"
-          >
-            Automatic audio file tagging tool using FreeDB for Windows
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.6}>
-          <WorkGridItem id="amembo" thumbnail={thumbAmembo} title="Amembo">
-            P2P private file sharing tool with MSN Messenger integration for
-            Windows
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.5}>
+            <WorkGridItem
+              id="pichu2"
+              thumbnail={thumbPichu2}
+              title="Pichu*Pichu"
+            >
+              Twitter client app for iPhone Safari
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.5}>
+            <WorkGridItem
+              id="freedbtagger"
+              thumbnail={thumbFreeDBTagger}
+              title="freeDBTagger"
+            >
+              Automatic audio file tagging tool using FreeDB for Windows
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.6}>
+            <WorkGridItem id="amembo" thumbnail={thumbAmembo} title="Amembo">
+              P2P private file sharing tool with MSN Messenger integration for
+              Windows
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   );
 };
 
