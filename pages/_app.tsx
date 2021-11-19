ce64e5506633044a1bd5ea90d8e0c2c190820735
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-11-20 04:09:23 +0600

    changed the name of main.tsx to Main.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import Layout from '../components/layouts/main';
+import Layout from '../components/layouts/Main';
 import theme from '../lib/theme';
 import Fonts from '../components/Fonts';
 import { AnimatePresence } from 'framer-motion';
